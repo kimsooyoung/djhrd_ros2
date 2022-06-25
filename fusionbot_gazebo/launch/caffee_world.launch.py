2d3ff135bd9f5257676acd0363cafa0ba934ce1d
kimsooyoung <tge1375@naver.com>
2022-06-26 02:54:13 +0900

    Update : bot added

--- fusionbot_gazebo/launch/caffee_world.launch.py
+++ fusionbot_gazebo/launch/caffee_world.launch.py
@@ -45,7 +45,53 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py'))
     )
 
+    # Robot State Publisher
+    urdf_file = os.path.join(description_pkg_path, 'urdf', 'fusionbot.urdf')
+
+    doc = xacro.parse(open(urdf_file))
+    xacro.process_doc(doc)
+    robot_description = {'robot_description': doc.toxml()}
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    # Joint State Publisher
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    # Spawn Robot
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'ip010',
+            '-x', '3.2',
+            '-y', '9.0',
+            '-z', '0.19',
+            '-Y', '-1.5707',
+            # '-x', '6.0',
+            # '-y', '2.2',
+            # '-z', '0.1',
+            # '-R', '0.0',
+            # '-P', '0.0',
+            # '-Y', '3.14',
+        ],
+        output='screen'
+    )
+
+
     return LaunchDescription([
         start_gazebo_server_cmd,
         start_gazebo_client_cmd,
+        robot_state_publisher,
+        joint_state_publisher,
+        spawn_entity,
     ])
