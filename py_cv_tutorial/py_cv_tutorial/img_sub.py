3ba7599e36747ae99c2b33453f446cc680fd33b3
kimsooyoung <tge1375@naver.com>
2022-10-27 01:20:02 +0900

    cv example done

--- py_cv_tutorial/py_cv_tutorial/img_sub.py
+++ py_cv_tutorial/py_cv_tutorial/img_sub.py
@@ -9,7 +9,9 @@ import rclpy # Python library for ROS 2
 from rclpy.node import Node # Handles the creation of nodes
 from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+import numpy as np
 import cv2 # OpenCV library
+
 
 class ImageSubscriber(Node):
     """
@@ -26,13 +28,69 @@ class ImageSubscriber(Node):
         # from the video_frames topic. The queue size is 10 messages.
         self.subscription = self.create_subscription(
             Image, 
-            'video_frames', 
+            'camera/color/image_raw', 
             self.listener_callback, 
             10)
         self.subscription # prevent unused variable warning
             
         # Used to convert between ROS and OpenCV images
         self.br = CvBridge()
+
+    def hough_transform(self, img_frame):
+        gray = cv2.cvtColor(img_frame, cv2.COLOR_RGB2GRAY)
+
+        # Define a kernel size and apply Gaussian smoothing
+        kernel_size = 5
+        blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+
+        # Define our parameters for Canny and apply
+        low_threshold = 100
+        high_threshold = 200
+        edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+        # Next we'll create a masked edges image using cv2.fillPoly()
+        mask = np.zeros_like(edges)
+        ignore_mask_color = 255
+
+        # This time we are defining a four sided polygon to mask
+        imshape = img_frame.shape # 640 * 480
+        print(imshape)
+        vertices = np.array(
+            [[(0, imshape[0]), 
+            (imshape[1] / 2 - 20, imshape[0] / 2), 
+            (imshape[1] / 2 + 20, imshape[0] / 2), 
+            (imshape[1], imshape[0])]],
+            dtype=np.int32,
+        )
+        cv2.fillPoly(mask, vertices, ignore_mask_color)
+        masked_edges = cv2.bitwise_and(edges, mask)
+
+        # Define the Hough transform parameters
+        # Make a blank the same size as our image to draw on
+        rho = 2  # distance resolution in pixels of the Hough grid
+        theta = np.pi / 180  # angular resolution in radians of the Hough grid
+        threshold = 40  # minimum number of votes (intersections in Hough grid cell)
+        min_line_length = 100  # minimum number of pixels making up a line
+        max_line_gap = 40  # maximum gap in pixels between connectable line segments
+        line_image = np.copy(img_frame) * 0  # creating a blank to draw lines on
+
+        # Run Hough on edge detected image
+        # Output "lines" is an array containing endpoints of detected line segments
+        lines = cv2.HoughLinesP(
+            masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap
+        )
+
+        # Iterate over the output "lines" and draw lines on a blank image
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+        # Create a "color" binary image to combine with line image
+        color_edges = np.dstack((edges, edges, edges))
+
+        # Draw the lines on the edge image
+        lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0)
+        return lines_edges
    
     def listener_callback(self, data):
         """
@@ -42,10 +100,11 @@ class ImageSubscriber(Node):
         self.get_logger().info('Receiving video frame')
 
         # Convert ROS Image message to OpenCV image
-        current_frame = self.br.imgmsg_to_cv2(data)
+        current_frame = self.br.imgmsg_to_cv2(data, "bgr8")
+        edge_frame = self.hough_transform(current_frame)
 
         # Display image
-        cv2.imshow("camera", current_frame)
+        cv2.imshow("camera", edge_frame)
 
         cv2.waitKey(1)
 
