db1e51ba39550d5544c5ef6a2ec5e8f5316d0bf7
kimsooyoung <tge1375@naver.com>
2022-10-29 05:33:23 +0900

    Desc editted

--- src_description/launch/src_description.launch.py
+++ src_description/launch/src_description.launch.py
@@ -16,20 +16,7 @@ def generate_launch_description():
 
     pkg_path = os.path.join(get_package_share_directory("src_description"))
     rviz_config_file = os.path.join(pkg_path, "rviz", "description.rviz")
-    xacro_file = os.path.join(pkg_path, "urdf", "src_body.urdf.xacro")
-
-    # Get URDF via xacro
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            xacro_file,
-            " ",
-            "prefix:=",
-            '""',
-        ]
-    )
-    robot_description = {"robot_description": robot_description_content}
+    urdf_file = os.path.join(pkg_path, "urdf", "src_body.urdf")
 
     joint_state_publisher_gui = Node(
         package="joint_state_publisher_gui",
@@ -38,10 +25,12 @@ def generate_launch_description():
     )
 
     robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        parameters=[robot_description],
+        package='robot_state_publisher',
+        node_executable='robot_state_publisher',
+        node_name='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': True}],
+        arguments=[urdf_file],
     )
 
     # Launch RViz
--- src_gazebo/launch/empty_world.launch.py
+++ src_gazebo/launch/empty_world.launch.py
@@ -177,17 +177,11 @@ def generate_launch_description():
                 on_exit=[src_gazebo_controller],
             )
         ),
-        # RegisterEventHandler(
-        #     event_handler=OnProcessExit(
-        #         target_action=load_velocity_controller,
-        #         on_exit=[src_odometry],
-        #     )
-        # ),
 
-        TimerAction(    
-            period=7.0,
-            actions=[rviz]
-        ),
+        # TimerAction(    
+        #     period=7.0,
+        #     actions=[rviz]
+        # ),
 
         start_gazebo_server_cmd,
         start_gazebo_client_cmd,
